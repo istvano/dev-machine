d9c30b395c827513ccc48297894241a23afd11b1
Istvan Orban <istvan.orbann@gmail.com>
2023-12-11 07:41:34 +0000

    inventory removed

---
+++ scripts/local.mk
@@ -0,0 +1,9 @@
+.PHONY: local/inventory
+local/inventory:  ##@local Create inventory file for local machine
+	@VM_IP=$(LOCAL_ADDRESS) ; \
+	$(SED) -e "s/--ip--/$$VM_IP/g" -e "s/--user--/$(VM_USER)/g" $(PLYBK)/inventory.tmpl > $(PLYBK)/inventory
+
+.PHONY: local/install-public-key
+local/install-public-key:  ##@local Install public key for local machine
+	@VM_IP=$(LOCAL_ADDRESS) ; \
+	$(SSH) -o PubkeyAuthentication=no -o PreferredAuthentications=password $(VM_USER)@$$VM_IP sh -c "'cat >> .ssh/authorized_keys'" < $(DEV_KEY).pub
