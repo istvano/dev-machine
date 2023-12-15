d38c79f99849da2a85e31247b746995d906d5e8a
istvano <istvan.orban@gmail.com>
2023-12-15 11:37:58 +0100

    add hoppscotch and remove postman

--- scripts/dev.mk
+++ scripts/dev.mk
@@ -1,6 +1,15 @@
 
 
-.PHONY: init
+.PHONY: dev/rest/editor
 dev/rest/editor: ## Running hoppscotch
 	$(DOCKER) compose -f ./etc/hoppscotch/docker-compose.yml up
 
+.PHONY: dev/ufw/allow
+dev/ufw/allow: ## Allow hoppscotch proxy to localhost
+	@SUBNET=$(shell docker network inspect hoppscotch_default --format "{{(index .IPAM.Config 0).Subnet}}"); \
+	sudo ufw allow from $$SUBNET
+
+.PHONY: dev/ufw/deny
+dev/ufw/deny: ## Deny hoppscotch proxy to localhost
+	@SUBNET=$(shell docker network inspect hoppscotch_default --format "{{(index .IPAM.Config 0).Subnet}}"); \
+	sudo ufw delete allow from $$SUBNET
